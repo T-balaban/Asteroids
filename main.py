1793f80f66c8118168a20448a9ae42e5202613c0
T-balaban <tolgaabalaban@gmail.com>
2025-11-06 00:42:21 +0300

    Moving added

--- main.py
+++ main.py
@@ -3,27 +3,28 @@
 # throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        dt = clock.tick(60) / 1000
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        
         player.update(dt)
+
+        screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
-    # print("Starting Asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
